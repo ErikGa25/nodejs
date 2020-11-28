41631eb529d74b7f088a6db14c557bcb0d58cc24
ErikGa25 <jerik67@gmail.com>
2020-11-27 20:00:40 -0600

    se agrego funcion para cargar estilos css y archivos js

--- servidor_node/app.js
+++ servidor_node/app.js
@@ -3,10 +3,41 @@ let fs   = require('fs')
 let path = require('path');
 
 let servidor = http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-type' : 'text/html'});
-    response.write('<h3>Aplicación Web con Node JS</h3>');
-    console.log('Cargando contenido...');
-    response.end();
+    let filePath = request.url;
+    let contentType = '';
+    
+    if (filePath == '/') {
+        filePath = '/public/views/index.html';
+    }
+
+    filePath = __dirname+filePath;
+    fileExtension = path.extname(filePath);
+
+    switch (fileExtension) {
+        case ".css":
+            contentType = "text/css";
+            break;
+
+        case ".js":
+            contentType = "text/javascript";
+            break;
+
+        case ".html":
+            contentType = "text/html";
+            break;
+    }
+
+    fs.readFile(filePath, (error, content)=>{
+        if(!error) {
+            response.writeHead(200, {"Content-Type": contentType});
+            response.write(content);
+            response.end();
+        } else {
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.write('Not Found 404');
+            response.end();
+        }
+    });
 });
 
 servidor.listen(3000);
