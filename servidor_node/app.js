a29ff2223accf0faf89fe85d68b3ecac71850e71
ErikGa25 <jerik67@gmail.com>
2020-11-27 19:34:17 -0600

    Se creo estructura del proyecto

--- servidor_node/app.js
+++ servidor_node/app.js
@@ -1,7 +1,9 @@
-var http = require('http');
+let http = require('http');
+let fs   = require('fs')
+let path = require('path');
 
-var servidor = http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-type' : 'text/html;charset=utf-8'});
+let servidor = http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-type' : 'text/html'});
     response.write('<h3>Aplicación Web con Node JS</h3>');
     console.log('Cargando contenido...');
     response.end();
