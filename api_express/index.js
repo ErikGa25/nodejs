59b85e6c9cfc613816cfa27a82e3ee2b58765730
ErikGa25 <jerik67@gmail.com>
2020-11-29 12:30:09 -0600

    Se agregaron las rutas del API

--- api_express/index.js
+++ api_express/index.js
@@ -11,13 +11,44 @@ app.use(bodyParser.json());
 let connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 't1erik6',
+    password : '',
     database : 'node20_mysql'
 });
 
 connection.connect(error => {
     if(error) throw error;
     console.log('DB funcionando ...');
+});
+
+// Rutas de la API
+// inicio
+app.get('/', (request, response) => {
+    response.send('Bienvenido al API de Node JS');
+});
+
+// listar todos los registros
+app.get('/all', (request, response) => {
+    response.send('Todos');
+});
+
+// listar un registro
+app.get('/one/:id', (request, response) => {
+    response.send('Uno');
+});
+
+// agregar un registro
+app.post('/add', (request, response) => {
+    response.send('Agregar');
+});
+
+// actualizar un registro
+app.put('/update/:id', (request, response) => {
+    response.send('Actualizar');
+});
+
+// eliminar un registro
+app.delete('/delete/:id', (request, response) => {
+    response.send('Eliminar');
 });
 
 app.listen(PORT, () => console.log('Servidor iniciado ....'));
